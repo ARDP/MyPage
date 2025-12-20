7aeeabadd3829817207581b34ecf5fa2a62c090b
Alexandrei <alexandreirdp@gmail.com>
2025-12-20 01:02:55 -0400

    fix tailwind prod

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,ts,jsx,tsx}",
+    "./src/styles/global.css"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
